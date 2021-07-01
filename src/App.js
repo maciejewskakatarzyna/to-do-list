30133e29e2c270b8d68b23b531f6b0a1dfd69f8d
kasiamaciejewska <maciejewska.kontakt@gmail.com>
2021-07-01 21:35:39 +0200

    save todos in local storage

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from "react";
 import {TodoRow} from "./TodoRow";
 import {TodoBanner} from "./TodoBanner";
 import {TodoCreator} from "./TodoCreator";
+import {VisibilityControl} from "./VisibilityControl";
 
 export default class App extends Component {
 
@@ -13,7 +14,7 @@ export default class App extends Component {
                   {action: "Wziąć buty", done: false},
                   {action: "Zebrać bilety", done: true},
                   {action: "Zadzwonić do Jurka", done: false }],
-      // newItemText: ""
+      showCompleted: true
     }
   }
 
@@ -26,7 +27,7 @@ export default class App extends Component {
       this.setState({
         todoItems: [...this.state.todoItems,
           { action: task, done: false }],
-      });
+      }, () => localStorage.setItem("todos", JSON.stringify(this.state)));
     }
   }
 
@@ -36,8 +37,24 @@ export default class App extends Component {
     ? { ...item, done: !item.done} : item)
   });
 
-  todoTableRows = () => this.state.todoItems.map(item =>
- <TodoRow key={item.action} item={item} callback={this.toggleTodo}/>)
+  todoTableRows = (doneValue) => this.state.todoItems
+      .filter(item => item.done === doneValue).map(item =>
+      <TodoRow key={item.action} item={item} callback={this.toggleTodo}/>)
+
+  componentDidMount = () => {
+    let data = localStorage.getItem("todos")
+    this.setState(data != null
+    ? JSON.parse(data)
+    : {
+      userName: "Adam",
+          todoItems: [{ action: "Kupić kwiaty", done: false},
+            {action: "Wziąć buty", done: false},
+            {action: "Zebrać bilety", done: true},
+            {action: "Zadzwonić do Jurka", done: false}],
+          showCompleted: true
+        })
+  }
+
 
 
   render = () =>
@@ -46,11 +63,23 @@ export default class App extends Component {
       <div className="container-fluid">
         <TodoCreator callback={this.createNewTodo}/>
         <table className="table table-striped table-bordered">
-          <head>
+          <thead>
             <tr><th>Opis</th><th>Wykonane</th></tr>
-          </head>
-          <tbody>{this.todoTableRows()}</tbody>
+          </thead>
+          <tbody>{this.todoTableRows(false)}</tbody>
         </table>
+        <div className="bg-secondary text-white text-center p-2">
+          <VisibilityControl description="wykonanie zadania"
+            isChecked={this.state.showCompleted}
+            callback={(checked) => this.setState({showCompleted: checked})}/>
+        </div>
+        {this.state.showCompleted &&
+        <table className="table table-striped table-bordered">
+          <thead>
+          <tr><th>Opis</th><th>Wykonane</th></tr>
+          </thead>
+          <tbody>{this.todoTableRows(true)}</tbody>
+        </table> }
       </div>
 
     </div>
